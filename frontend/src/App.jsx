d0a281fc83e1a51a05e84226a45f3b299ee0675a
henryqbonilla <54089523+henryqbonilla@users.noreply.github.com>
2024-09-04 21:58:30 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Dashboard1 from './components/Dashboard1'
+import Dashboard2 from './components/Dashboard2'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar
+        content = {
+          <Routes>
+            <Route path='' element={<Dashboard1/>}/>
+            <Route path='/dashboard2' element={<Dashboard2/>}/>
+          </Routes>
+        }
+      />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Dashboard1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard1 = () => {
+    return (
+        <div>This is the dashboard 1 page</div>
+    )
+}
+
+export default Dashboard1
